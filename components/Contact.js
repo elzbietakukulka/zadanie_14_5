073fd26fd3d63b0d767c273f3558c6fc43fd1e73
Elżbieta Kukułka <ellakaras@gmail.com>
2018-04-04 18:49:06 +0200

    Task 14_5

---
+++ components/Contact.js
@@ -0,0 +1,25 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem row'},
+        React.createElement('div', {className: 'col-4'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
+          })
+        ),
+        React.createElement('div', {className: 'col-8'},
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            'Email: ' + this.props.item.email
+          )
+        )
+      )
+      )
+  }
+});
